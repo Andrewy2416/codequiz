6e3a164f3a0ae6f86b641979c80a3ba36a668a2e
Andrewy2416 <andrewy2416@gmail.com>
2021-10-17 00:37:26 -0700

    added base var. added functions for local storage including final scorr, getting score, and adding a function for clearing score.

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 var questions = [{
     title: "Commonly used data types DO NOT include..",
-    choices: ["strings( )", "booleans( )", "alerts( )", "numbers( )"],
-    answer: "alerts( )"
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
 },
 {
     title: "String values must be enclosed within ____ when being assigned to variables.",
-    choices: ["commas( )", "curly brackets( )", "quotes( )", "parenthesis( )"],
-    answer: "quotes( )"
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes"
 },
 {
     title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes( )", "curly brackets( )", "parentheses( )", "square brackets( )"],
-    answer: "parentheses( )"
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
 },
 {
     title: "Arrays in Javascript can be used to store ____.",
-    choices: ["numbers and strings( )", "other arrays( )", "booleans( )", "all of the above( )"],
-    answer: "all of the above( )"
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
 },
 {
     title: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
-    choices: ["last( )", "put( )", "push( )", "pop( )"],
-    answer: "push( )"
+    choices: ["last", "put", "push", "pop"],
+    answer: "push"
 }
 ]
+
+var score = 0;
+var currentQuestion = -1;
+var timeLeft = 0;
+var timer;
 
 function start() {
 
@@ -42,3 +47,45 @@ function start() {
 
     next();
 }
+
+//end's the game when timer runs out and added textcontent for when end game window shows up
+function endGame() {
+    clearInterval(timer);
+    
+    var quizContent = `
+    <h2>Game over!</h2>
+    <h3>You got a ` + score +  ` /100!</h3>
+    <h3>That means you got ` + score / 20 +  ` questions correct!</h3>
+    <input type="text" id="name" placeholder="First name"> 
+    <button onclick="setScore()">Set score!</button>`;
+    
+    document.getElementById("quizBody").innerHTML = quizContent;
+    }
+    
+//local storage functions
+function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName",  document.getElementById('name').value);
+    getScore();
+    }
+    
+    
+    function getScore() {
+    var quizContent = `
+    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("highscore") + `</h1><br> 
+    
+    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+    
+    `;
+    
+    document.getElementById("quizBody").innerHTML = quizContent;
+    }
+    
+    //function to clear score from local storage
+    function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName",  "");
+    
+    resetGame();
+    }
